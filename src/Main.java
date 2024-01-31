2e940485eea203a42cc4d0221dad89072c63e29c
SystemX <monster_of_itameru@mail.ru>
2024-01-31 20:50:35 +0300

    Устранил недостатки по работе

--- src/Main.java
+++ src/Main.java
@@ -41,25 +41,38 @@ public class Main {
         } else if (age2 >= 14) {
             System.out.println("Если возраст ребенка равен " + age2 + " то ему можно кататься на аттракционе без сопровождения");
         }
+
         // задача 6
-        int places = 103;
-        if (places < 60) {
-            System.out.println("В вагоне есть и сидячее места и стоячее");
-        } else if (places > 60 && places < 102) {
+        int places = 62;
+        int places2 = 32;
+        if (places < 60 && places2 < 42) {
+            System.out.println("В вагоне есть и сидячие и стоячие места");
+        } else if (places >= 60 && places2 < 42) {
             System.out.println("В вагоне есть только стоячее места");
-        } else if (places > 102) {
+        } else if (places < 60 && places2 >= 42) {
+            System.out.println("В вагоне есть только сидячие места");
+        } else if (places >= 60 && places2 >= 42) {
             System.out.println("В вагоне нет свободных места");
         }
         // задача 7
-        int one = 50;
-        int two = 43;
-        int three = 46;
+        int one = 80;
+        int two = 70;
+        int three = 90;
         if (one > two && one > three) {
             System.out.println("Первое число больше второго и третьего");
         } else if (two > one && two > three) {
             System.out.println("Второе число больше первого и третьего");
         } else  if (three > one && three > two) {
             System.out.println("Третье число больше второго и первого");
+        } else if (one >= two && one > three) {
+            System.out.println("первое и второе больше третьего");
+        } else if (two >= three && two > one) {
+                System.out.println("второе и третье число больше первого");
+        } else if (one >= three && one > two) {
+            System.out.println("первое и третье больеш второго");
+        } else if(one >= two && two >= three) {
+            System.out.println("все числа равны");
+
         }
 
 
